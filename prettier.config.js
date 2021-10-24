1d54f1da7fc6af3b95180b248b6e58ce66829718
Antoine <contact@antoinelin.com>
2021-10-24 20:16:16 +0200

    chore: initial boiler config

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  parser: "typescript",
+  printWidth: 100,
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: "avoid"
+}
